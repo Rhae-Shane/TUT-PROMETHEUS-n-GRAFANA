0d6b1a7d4415999a2b8f2ecdaf68297d9ad0fc02
Rhae-Shane <omeshkumar9813499778@gmail.com>
2026-04-20 00:13:24 +0530

    project step with basic prometheus

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import { middleware } from "./middleware";
+import client from "prom-client";
+import { metricsMiddleware } from "./metrics";
+
+const app = express();
+
+app.use(express.json());
+app.use(middleware);
+app.use(metricsMiddleware);
+
+app.get("/user", async (req, res) => {
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    res.send({
+        name: "John Doe",
+        age: 25,
+    });
+});
+
+app.post("/user", (req, res) => {
+    const user = req.body;
+    res.send({
+        ...user,
+        id: 1,
+    });
+});
+
+app.get('/cpu', async (req, res) => {
+    await new Promise(resolve => setTimeout(resolve, Math.random() * 50)); // Simulate a CPU intensive task
+    res.send('CPU intensive task completed');
+});
+
+
+app.get("/metrics", async (req, res) => {
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
+})
+
+app.listen(3000);
